6e70f2d7748697515258cdb9e161a49ae0d035af
Bor P <golovanovaea.7@gmail.com>
2022-12-21 19:54:25 +0300

    programm

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// вводим тип переменной, пробел, имя переменной = . Обязательно!! вводимые данные и выходные должны быть одинаковыми.
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqr = number * number;
+
+int sqrMath = Convert.ToInt32(Math.Pow(number, 5));
+
+System.Console.WriteLine(sqr);
+
+System.Console.WriteLine(sqrMath);
